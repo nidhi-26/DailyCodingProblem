ad1bc31588b90801f295833d967b304cc41ae3cd
nidhi-26 <32599084+nidhi-26@users.noreply.github.com>
2020-01-25 16:42:18 +0530

    Create #5_ReturnEitherOfPair.py
    cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4

---
+++ #5_ReturnEitherOfPair.py
@@ -0,0 +1,13 @@
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(cons):
+    return cons(lambda a, b: a)
+
+def cdr(cons):
+    return cons(lambda a, b: b)
+
+print(car(cons(3, 4)))
+print(cdr(cons(3, 4)))
